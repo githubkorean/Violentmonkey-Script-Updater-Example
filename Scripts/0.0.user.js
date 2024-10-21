a9a302310c26c7914112ae17ff295d28afbeb639
githubkorean <100678096+githubkorean@users.noreply.github.com>
2024-10-22 00:31:07 +0900

    Update 0.0.user.js

--- Scripts/0.0.user.js
+++ Scripts/0.0.user.js
@@ -5,7 +5,7 @@
 // @homepageURL   https://github.com/githubkorean/Violentmonkey-Script-Updater-Example
 // @match         *://*/*
 // @version       0.0
-// @description   테스트용 0.1버전 스크립트
+// @description   테스트용 0.0버전 스크립트
 // @icon          https://www.google.com/s2/favicons?sz=256&domain_url=github.com
 // @author        mickey90427 <mickey90427@naver.com>
 // @require       https://github.com/githubkorean/Violentmonkey-Script-Updater-Example/raw/refs/heads/main/Updater.js
