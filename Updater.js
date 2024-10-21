bdaee94f622f2318d121c6e95be3b171901d9134
githubkorean <100678096+githubkorean@users.noreply.github.com>
2024-10-22 00:33:12 +0900

    Update Updater.js

--- Updater.js
+++ Updater.js
@@ -8,7 +8,7 @@ async function checkForUpdates(repo, currentVersion) {
     const lastNoShow = await GM.getValue('version_no_show');
     const now = new Date().getTime();
 
-    // 5초 동안 무시한 경우, 아무것도 표시하지 않음
+    // 1분 동안 무시한
     if (lastIgnored && (now - lastIgnored < 5 * 1000)) {
         // 남은 시간 계산
         const remainingTime = 5 * 1000 - (now - lastIgnored);
@@ -16,7 +16,7 @@ async function checkForUpdates(repo, currentVersion) {
         return; 
     }
 
-    // 다음날 표시할 시간인지 확인
+    // 24시간 동안 무시
     if (lastNoShow && now < lastNoShow) {
         const remainingTime = lastNoShow - now;
         console.log(`다음날 다시 나타나기까지 ${formatTime(remainingTime)} 남았습니다.`);
