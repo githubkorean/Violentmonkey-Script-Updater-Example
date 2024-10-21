d8d532d0ff8746bc55561efd4aaa411a8139e4dd
githubkorean <100678096+githubkorean@users.noreply.github.com>
2024-10-21 23:33:01 +0900

    Create 0.0.user.js

---
+++ Scripts/0.0.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name          업데이트 테스트 0.0
+// @namespace     https://github.com/githubkorean/Violentmonkey-UpdateChecker
+// @supportURL    https://github.com/githubkorean/Violentmonkey-UpdateChecker
+// @homepageURL   https://github.com/githubkorean/Violentmonkey-UpdateChecker
+// @match         *://*/*
+// @version       0.0
+// @description   테스트용 0.0버전 스크립트
+// @icon          https://www.google.com/s2/favicons?sz=256&domain_url=github.com
+// @author        mickey90427 <mickey90427@naver.com>
+// @require       https://github.com/githubkorean/Violentmonkey-Script-Updater-Example/raw/refs/heads/main/Updater.js
+// @grant         GM.setValue
+// @grant         GM.getValue
+// @grant         GM.xmlHttpRequest
+// @grant         GM_openInTab
+// @license       MIT
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    checkForUpdates('githubkorean/Violentmonkey-Script-Updater-Example', GM_info.script.version);
+})();
